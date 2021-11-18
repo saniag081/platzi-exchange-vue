9c316bbe6904df2b9f1f9198ec6994a51f18d7a6
Santiago G <sgonzalezhernandez081@gmail.com>
2021-11-18 11:26:01 -0500

    feat(*): add vue-router

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,18 @@
 <template>
   <main>
     <px-header />
-    <px-assets-table />
+
+    <router-view class="container px-5 sm:px-20 py-20 flex justify-center" />
   </main>
 </template>
 
 <script>
 import PxHeader from '@/components/PxHeader'
-import PxAssetsTable from './components/PxAssetsTable.vue'
 
 export default {
   name: 'App',
   components: {
-    PxHeader,
-    PxAssetsTable
+    PxHeader
   }
 }
 </script>
---
+++ src/views/Home.vue
@@ -0,0 +1,14 @@
+<template>
+    <px-assets-table />
+</template>
+
+<script>
+import PxAssetsTable from '@/components/PxAssetsTable.vue'
+
+export default {
+    name: 'Home',
+
+    components: { PxAssetsTable}
+}
+
+</script>
